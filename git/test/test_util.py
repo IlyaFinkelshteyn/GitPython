5c5fdce10fcf4b82392159fe39d41191eea97d81
Kostis Anagnostopoulos <ankostis@gmail.com>
2016-09-26 12:32:28 +0200

    test, #519: Popen() universal_newlin.es NoWindow in Winfoes
    + More win-fixes:
      + Do not check unicode files in < py3.
      + util, #519: x3 timeout of lock-file blocking, failing in Appveyor.

--- git/test/test_util.py
+++ git/test/test_util.py
@@ -90,7 +90,7 @@ class TestUtils(TestBase):
         wait_lock = BlockingLockFile(my_file, 0.05, wait_time)
         self.failUnlessRaises(IOError, wait_lock._obtain_lock)
         elapsed = time.time() - start
-        assert elapsed <= wait_time + 0.02, elapsed  # some extra time it may cost
+        self.assertLess(elapsed, wait_time + 0.08)  # More extra time costs, but...
 
     def test_user_id(self):
         assert '@' in get_user_id()
